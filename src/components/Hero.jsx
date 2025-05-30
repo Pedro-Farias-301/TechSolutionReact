855295ae8d3378a6d64c3cb37445500993269a4f
Pedro <pedrobarbosafarias9696@gmail.com>
2025-05-30 20:46:37 -0300

    Teste

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
+import { useLocation } from 'react-router-dom';
+
 function Hero() {
+  const location = useLocation(); 
+  const isAboutPage = location.pathname === '/about'; 
+
   return (
     <div
-      className="hero min-h-screen"
+      className="hero min-h-[50vh]"
       style={{
         backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
       }}
@@ -13,21 +18,24 @@ function Hero() {
           <p className="mb-5">
             Uma plataforma projetada para conectar estudantes, comunidade e mentores, criando soluções e oportunidades para todos. Se tiver dúvidas, deixe seu contato!
           </p>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="label">Nome</label>
-                <input type="text" className="input" placeholder="Nome" />
-                <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
-                <label className="label">Telefone</label>
-                <input type="number" className="input" placeholder="Telefone" />
-                <div className="flex gap-2 mt-4">
-                  <button className="btn btn-neutral flex-1">Enviar</button>
-                </div>
-              </fieldset>
+          {/* Condicionalmente renderizar o formulário */}
+          {!isAboutPage && (
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <fieldset className="fieldset">
+                  <label className="label">Nome</label>
+                  <input type="text" className="input" placeholder="Nome" />
+                  <label className="label">Email</label>
+                  <input type="email" className="input" placeholder="Email" />
+                  <label className="label">Telefone</label>
+                  <input type="number" className="input" placeholder="Telefone" />
+                  <div className="flex gap-2 mt-4">
+                    <button className="btn btn-neutral flex-1">Enviar</button>
+                  </div>
+                </fieldset>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
